d19964d46ac45a7ab24d3698e8c2391952368f65
GG-java <719317963@qq.com>
2020-06-21 00:58:10 +0800

    test

--- src/main/java/com/xl/controller/HelloController.java
+++ src/main/java/com/xl/controller/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
     @ResponseBody
     @RequestMapping("hello")
     public String hello(){
-        return "hello world dsa11 test";
+        return "hello world dsa11 test1212";
     }
 }
