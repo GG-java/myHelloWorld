3aa9b4470b68a1d01cfbb54fa841f23180201040
GG-java <719317963@qq.com>
2020-06-20 20:08:32 +0800

    上传测试git  06

--- src/main/java/com/xl/controller/HelloController.java
+++ src/main/java/com/xl/controller/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
     @ResponseBody
     @RequestMapping("hello")
     public String hello(){
-        return "hello world ttttttttttttttttttttt";
+        return "hello world tttttttttttttttt";
     }
 }
