f828cbfc7f57cd8436b1c98aab1da8c52cee378a
imsgev <719317963@qq.com>
2020-06-20 20:43:32 +0800

    上传测试git 001

--- src/main/java/com/xl/controller/HelloController.java
+++ src/main/java/com/xl/controller/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
     @ResponseBody
     @RequestMapping("hello")
     public String hello(){
-        return "hello world dsa0";
+        return "hello world dsa11";
     }
 }
